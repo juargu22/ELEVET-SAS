2aebded00207c44e4ad05857fa459c2a279e39b4
Julian <juargu22@gmail.com>
2020-07-31 17:00:54 -0500

    correcrion direccion e-mail

--- correo.php
+++ correo.php
@@ -1,7 +1,7 @@
 <?php
 
     //esto es al correo destino
-    $destinatario = 'juargu22@gmail.com';
+    $destinatario = 'contacto@elevetsas.com';
     $nombre = $_POST['nombre'];
     $empresa = $_POST['empresa'];
     $email = $_POST['email'];
@@ -9,9 +9,10 @@
     $mensaje = $_POST['mensaje'];
 
     //Datos para el Correo
-    $destinatario = "elevet.sas@gmail.com";
-    $asunto = "Contacto desde web";
+    $destinatario = "contacto@elevetsas.com";
+    $asunto = "Contacto desde pagina web";
 
+    $carta .= "Nuevo correro desde la pagina web: \n\n";
     $carta = "De: $nombre \n";
     $carta .= "Correo: $email \n";
     $carta .= "Tel: $tel\n";
@@ -21,29 +22,4 @@
     mail($destinatario, $asunto, $carta);
 
     header('Location:enviado.html');
-
-    // if(isset($_POST['enviar'])){
-    //   if (!empty($_POST['nombre']) &&
-    //       !empty($_POST['empresa']) &&
-    //       !empty($_POST['email']) &&
-    //       !empty($_POST['telefono']) &&
-    //       !empty($_POST['mensaje'])) {
-    //
-    //        $nombre = $_POST['nombre'];
-    //        $empresa = $_POST['empresa'];
-    //        $email = $_POST['email'];
-    //        $tel = $_POST['telefono'];
-    //        $mensaje = $_POST['mensaje'];
-    //        $header = "From: juargu22@gmail.com" . "\r\n";
-    //        $header. = "Reply-to: juargu22@gmail.com" . "\r\n";
-    //        $header = "X-Mailer: PHP/" . phpversion();
-    //        $mail = @mail($email, $empresa, $mensaje, $telefono, $header);
-    //
-    //        if ($mail) {
-    //          echo "<h3> ¡Mail enviado con exito!, nos comunicaremos contigo lo antes posible.</h3>";
-    //        }
-    //   }
-    // }
-
-
  ?>
