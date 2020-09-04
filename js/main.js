6a8d0aebe7173a5e296e26c5775f272959cac76d
juargu22 <juargu22@gmail.com>
2020-09-03 21:26:44 -0500

    actualizacion mensaje inferior

--- js/main.js
+++ js/main.js
@@ -42,14 +42,3 @@ function mostrarScroll(){
 window.addEventListener('scroll', mostrarScroll);
 
 
-
-
-
-
-// seccion MAPA. se utiliza la libreria leaflet
-
-var map = L.map('mapa').setView([4.438452, -75.217679], 16);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
